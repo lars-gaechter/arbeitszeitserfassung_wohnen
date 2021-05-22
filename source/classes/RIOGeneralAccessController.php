9a68e82689f6dcb491ffab7f76049f130f440017
Lars Gaechter <l.gaechter@rafisa.ch>
2021-05-22 10:20:35 +0200

    Test die

--- source/classes/RIOGeneralAccessController.php
+++ source/classes/RIOGeneralAccessController.php
@@ -353,9 +353,10 @@ class RIOGeneralAccessController
         $user = $this->getUsers()->findOne(['sessionUsername' => $username]);
         $currentWorkDay = new RIOWorkDayObject();
         $allWorkDaysFromUser = $this->getWorkDaysByYearUser(RIODateTimeFactory::getDateTime("01.".$monthYear)->format("Y"),$username)->find(["monthYear" => $monthYear])->toArray();
+        $return = $this->getPastWorkDaysUser($allWorkDaysFromUser, $currentWorkDay, $user);
         echo "test";
         die();
-        return $this->getPastWorkDaysUser($allWorkDaysFromUser, $currentWorkDay, $user);
+        return $return;
     }
 
     /**
