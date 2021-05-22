2d6b8dabff43040b61030343df5e7ed80b95d588
Lars Gaechter <l.gaechter@rafisa.ch>
2021-05-22 12:55:35 +0200

    Remove die

--- source/classes/RIOGeneralAccessController.php
+++ source/classes/RIOGeneralAccessController.php
@@ -353,7 +353,7 @@ class RIOGeneralAccessController
         $user = $this->getUsers()->findOne(['sessionUsername' => $username]);
         $currentWorkDay = new RIOWorkDayObject();
         $allWorkDaysFromUser = $this->getWorkDaysByYearUser(RIODateTimeFactory::getDateTime("01.".$monthYear)->format("Y"),$username)->find(["monthYear" => $monthYear])->toArray();
-        return $this->getPastWorkDaysUser($allWorkDaysFromUser, $currentWorkDay, $user);
+        return $this->getPastWorkDaysUser($allWorkDaysFromUser, $currentWorkDay, $user, true);
     }
 
     /**
