e66abd9967b373cb682341109410dffa06852fed
Lars Gaechter <l.gaechter@rafisa.ch>
2021-05-22 12:28:54 +0200

    Test die

--- source/classes/RIOGeneralAccessController.php
+++ source/classes/RIOGeneralAccessController.php
@@ -460,7 +460,6 @@ class RIOGeneralAccessController
             $OneWorkDayFromUserPastIndexed[] = $OneWorkDayFromUserPast;
             $i++;
         }
-        die();
         return $OneWorkDayFromUserPastIndexed;
     }
 
